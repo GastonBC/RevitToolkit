3ba8081ac0fa60e579dc2d724c6622cd83fd8bc7
GasBC <gastonbalparda@gmail.com>
2026-02-08 11:56:03 +1000

    Initial commit

---
+++ source/AddinLoader/Application.cs
@@ -0,0 +1,26 @@
+﻿using AddinLoader.Commands;
+using Nice3point.Revit.Toolkit.External;
+
+namespace AddinLoader
+{
+    /// <summary>
+    ///     Application entry point
+    /// </summary>
+    [UsedImplicitly]
+    public class Application : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            CreateRibbon();
+        }
+
+        private void CreateRibbon()
+        {
+            var panel = Application.CreatePanel("Commands", "AddinLoader");
+
+            panel.AddPushButton<StartupCommand>("Execute")
+                .SetImage("/AddinLoader;component/Resources/Icons/RibbonIcon16.png")
+                .SetLargeImage("/AddinLoader;component/Resources/Icons/RibbonIcon32.png");
+        }
+    }
+}
